a3ed57004c84011b335d443af7b1583351625231
Roman <www.ksel@gmail.com>
2024-11-26 20:52:49 +0300

    commit before merge

--- File1.py
+++ File1.py
@@ -1,1 +1,7 @@
-print('Hello world')
+password = input()
+
+
+if password[0].istitle() and password[-1].isdigit() and password != '' and len(password) >= 3 and password.isspace() == True:
+    print(True)
+else:
+    print(False)
